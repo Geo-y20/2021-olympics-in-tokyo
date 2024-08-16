e9cdb9005530d1ea595d1f9f5a59f7e2f483f55b
George Youhana <73796671+Geo-y20@users.noreply.github.com>
2024-08-16 15:21:22 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the saved Random Forest model
+with open('best_rf_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get input features from the form (you can adapt this to fit your form structure)
+    discipline = int(request.form['Discipline'])
+    event = int(request.form['Event'])
+    
+    # Prepare the features for prediction
+    features = np.array([[discipline, event]])
+    
+    # Make the prediction
+    prediction = model.predict(features)
+    
+    # Map the prediction to the medal type
+    medal_map = {0: 'Bronze', 1: 'Gold', 2: 'Silver'}
+    predicted_medal = medal_map.get(prediction[0], "Unknown")
+    
+    return render_template('index.html', prediction_text=f'The predicted medal type is: {predicted_medal}')
+
+if __name__ == "__main__":
+    app.run(debug=True)
